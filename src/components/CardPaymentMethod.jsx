907e814bc21acb9162c37614c03ef21001dd9122
Ibranista <techofreact@gmail.com>
2024-05-30 14:36:19 +0300

    chore: Fix formatting and naming conventions in FormSection and CardPaymentMethod components

--- src/components/CardPaymentMethod.jsx
+++ src/components/CardPaymentMethod.jsx
@@ -1,61 +1,76 @@
 import '../paymentMethod.css';
-import CustomInput from "../CustomInput"
-import { CardCvcElement, CardElement, CardExpiryElement, CardNumberElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import CustomInput from '../CustomInput';
+import {
+  CardCvcElement,
+  CardElement,
+  CardExpiryElement,
+  CardNumberElement,
+  useElements,
+  useStripe,
+} from '@stripe/react-stripe-js';
 import { useState } from 'react';
 
 function CardPaymentMethod({ values }) {
-    const elements = useElements();
-    const cardElement = elements?.getElement(CardNumberElement);
-    values.theCardElement = cardElement;
-    const stripe = useStripe();
-    console.log('the element==>',cardElement);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [success, setSuccess] = useState(false);
+  const elements = useElements();
+  const cardElement = elements?.getElement(CardNumberElement);
+  values.theCardElement = cardElement;
+  const stripe = useStripe();
+  console.log('the element==>', cardElement);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(false);
 
-    const CARD_ELEMENT_OPTIONS = {
-        style: {
-            base: {
-                iconColor: "black",
-                color: "black",
-                fontSize: "18px",
-                fontFamily: "Raleway, sans-serif",
-                fontSmoothing: "antialiased",
-                "::placeholder": {
-                    color: "black",
-                },
-            },
-            invalid: {
-                iconColor: "#fa004f",
-                color: "#fa004f",
-            },
+  const CARD_ELEMENT_OPTIONS = {
+    style: {
+      base: {
+        iconColor: 'black',
+        color: 'black',
+        fontSize: '18px',
+        fontFamily: 'Raleway, sans-serif',
+        fontSmoothing: 'antialiased',
+        '::placeholder': {
+          color: 'black',
         },
-    };
+      },
+      invalid: {
+        iconColor: '#fa004f',
+        color: '#fa004f',
+      },
+    },
+  };
 
-
-    
-
-    return (
-        <>
-            {values.paymentMethod === 'creditCard' && (
-                <>
-                    <CardNumberElement
-                        options={CARD_ELEMENT_OPTIONS}
-                    />
-                    <CardExpiryElement
-                        options={CARD_ELEMENT_OPTIONS}
-                    />
-                    <CardCvcElement
-                        options={CARD_ELEMENT_OPTIONS}
-                    />
-                </>
-            )}
-            <CustomInput
-                label="Special Instructions: " name="specialInstructions" type="textArea"
-                placeholder='Anything Else We Should Know? (i.e parking, entry, time flexibility, pets)'
+  return (
+    <>
+      {values.paymentMethod === 'creditCard' && (
+        <div className="card-elements">
+          <div className="card-number">
+            <label className="card-label">Card number</label>
+            <CardNumberElement
+              options={CARD_ELEMENT_OPTIONS}
+              class="card-number-input"
             />
-        </>
-    )
+          </div>
+
+          <div className="card-info">
+            <div className="expiry-date">
+              <label className="card-label">Expiration</label>
+              <CardExpiryElement options={CARD_ELEMENT_OPTIONS} />
+            </div>
+            <div className="cvc">
+              <label className="card-label">CVC</label>
+              <CardCvcElement options={CARD_ELEMENT_OPTIONS} />
+            </div>
+          </div>
+        </div>
+      )}
+      <CustomInput
+        label="Special Instructions: "
+        name="specialInstructions"
+        type="textArea"
+        placeholder="Anything Else We Should Know? (i.e parking, entry, time flexibility, pets)"
+      />
+    </>
+  );
 }
 
-export default CardPaymentMethod
+export default CardPaymentMethod;
