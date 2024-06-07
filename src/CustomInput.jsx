c89159b9e28bfb1cf28b9ee919110210eb1dd78e
Ibranista <techofreact@gmail.com>
2024-06-07 22:50:40 +0300

    style: Adjust margins and spacing in booking location and input styles

--- src/CustomInput.jsx
+++ src/CustomInput.jsx
@@ -1,80 +1,151 @@
 import propTypes from 'prop-types';
-import { ErrorMessage, useField } from "formik";
+import { ErrorMessage, useField } from 'formik';
 import CustomDatepicker from './components/CustomDatepicker';
 import TimePicker from './components/CustomTimepicker';
 
-const CustomInput = ({ label, placeholder, cityStateZip, dateOfBirth,availableDates,availableTimes, ...props }) => {
-    const [field, meta] = useField(props);
-    return (
-        <>
-            {
-                // if radio
-                props.type === 'radio' &&
-                <div className="radio-input-wrapper">
-                    <div className="radio-box">
-                        <input {...field} {...props} autoComplete="true" className='payment-radio-input' />
-                    </div>
-                    <p className="radio-input-label"
-                        onClick={() => field.onChange({ target: { name: field.name, value: field.value } })}
-                        style={{cursor:'pointer'}}
-                    >
-                        {label}
-                    </p>
-                </div>
-                // if text area
-                || props.type === 'textArea' &&
-                <div className="text-area-wrapper-input">
-                    <div className='text-area-label' htmlFor={field.name}>{label}</div>
-                    <textarea placeholder={placeholder} {...field} {...props} autoComplete="true" className='special-instructions-text-area' />
-                </div>
-            }
-            {(props.type !== 'radio' && props.type !== 'textArea' && props.type !== 'date' && props.type !== 'time') && (
-                <div className={cityStateZip ? 'cityStateZip' : 'input-box-wrapper'}>
-                    <div className="label-input-wrapper">
-                        <label className='input-box-label' htmlFor={field.name}>{label}</label>
-                        <input placeholder={placeholder} {...field} {...props} autoComplete="true" className={props.name === 'bookingAddress.address_1' ? 'input-box' : 'input-box'} />
-                    </div>
-                    <ErrorMessage className='input-box-error-message' component="div" name={field.name} style={errorMessage} />
-                </div>
-            )}
+const CustomInput = ({
+  label,
+  placeholder,
+  cityStateZip,
+  dateOfBirth,
+  availableDates,
+  availableTimes,
+  ...props
+}) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      {
+        // if radio
+        (props.type === 'radio' && (
+          <div className="radio-input-wrapper">
+            <div className="radio-box">
+              <input
+                {...field}
+                {...props}
+                autoComplete="true"
+                className="payment-radio-input"
+              />
+            </div>
+            <p
+              className="radio-input-label"
+              onClick={() =>
+                field.onChange({
+                  target: { name: field.name, value: field.value },
+                })
+              }
+              style={{ cursor: 'pointer' }}
+            >
+              {label}
+            </p>
+          </div>
+        )) ||
+          // if text area
+          (props.type === 'textArea' && (
+            <div className="text-area-wrapper-input">
+              <div className="text-area-label" htmlFor={field.name}>
+                {label}
+              </div>
+              <textarea
+                placeholder={placeholder}
+                {...field}
+                {...props}
+                autoComplete="true"
+                className="special-instructions-text-area"
+              />
+            </div>
+          ))
+      }
+      {props.type !== 'radio' &&
+        props.type !== 'textArea' &&
+        props.type !== 'date' &&
+        props.type !== 'time' && (
+          <div className={cityStateZip ? 'cityStateZip' : 'input-box-wrapper'}>
+            <div className="label-input-wrapper">
+              <label className="input-box-label" htmlFor={field.name}>
+                {label}
+              </label>
+              <input
+                placeholder={placeholder}
+                {...field}
+                {...props}
+                autoComplete="true"
+                className={
+                  props.name === 'bookingAddress.address_1'
+                    ? 'input-box'
+                    : 'input-box'
+                }
+              />
+            </div>
+            <ErrorMessage
+              className="input-box-error-message"
+              component="div"
+              name={field.name}
+              style={{}}
+            />
+          </div>
+        )}
 
-            {props.type === 'time' && (
-                <div className={cityStateZip ? 'cityStateZip' : 'input-box-wrapper'}>
-                    <div className="label-input-wrapper">
-                        <label className='input-box-label' htmlFor={field.name}>{label}</label>
-                        <TimePicker availableTimes={availableTimes} htmlFor={field.name} {...field} {...props} />
-                    </div>
-                    <ErrorMessage className='input-box-error-message' component="div" name={field.name} style={errorMessage} />
-                </div>
-            )}
+      {props.type === 'time' && (
+        <div className={cityStateZip ? 'cityStateZip' : 'input-box-wrapper'}>
+          <div className="label-input-wrapper">
+            <label className="input-box-label" htmlFor={field.name}>
+              {label}
+            </label>
+            <TimePicker
+              availableTimes={availableTimes}
+              htmlFor={field.name}
+              {...field}
+              {...props}
+            />
+          </div>
+          <ErrorMessage
+            className="input-box-error-message"
+            component="div"
+            name={field.name}
+            style={{}}
+          />
+        </div>
+      )}
 
-            {props.type === 'date' && (
-                <div className={cityStateZip ? 'cityStateZip' : 'input-box-wrapper'}>
-                    <div className="label-input-wrapper">
-                        <label className='input-box-label' htmlFor={field.name}>{label}</label>
-                        <CustomDatepicker availableDates={availableDates} dateOfBirth={dateOfBirth} htmlFor={field.name} {...field} {...props} />
-                    </div>
-                    <ErrorMessage className='input-box-error-message' component="div" name={field.name} style={errorMessage} />
-                </div>
-            )}
-
-        </>
-    )
-}
-
-const errorMessage = {
-    color: "red",
-    position: "absolute",
-    fontSize: "11px"
-}
+      {props.type === 'date' && (
+        <div className={cityStateZip ? 'cityStateZip' : 'input-box-wrapper'}>
+          <div className="label-input-wrapper">
+            <label className="input-box-label" htmlFor={field.name}>
+              {label}
+            </label>
+            <CustomDatepicker
+              availableDates={availableDates}
+              dateOfBirth={dateOfBirth}
+              htmlFor={field.name}
+              {...field}
+              {...props}
+            />
+          </div>
+          <ErrorMessage
+            className="input-box-error-message"
+            component="div"
+            name={field.name}
+            style={{}}
+          />
+        </div>
+      )}
+    </>
+  );
+};
+// const errorMessage = {
+//     color: "red",
+//     position: "absolute",
+//     fontSize: "11px"
+// }
 
 export default CustomInput;
 
 CustomInput.propTypes = {
-    label: propTypes.string.isRequired,
-    placeholder: propTypes.string,
-    cityStateZip: propTypes.bool,
-    props: propTypes.object,
-    ref: propTypes.object,
-    type: propTypes.string
-}
+  label: propTypes.string.isRequired,
+  placeholder: propTypes.string,
+  cityStateZip: propTypes.bool,
+  props: propTypes.object,
+  ref: propTypes.object,
+  type: propTypes.string,
+};
